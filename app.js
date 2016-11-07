bd6959050043e4674f8898e1dab8cec3de16a957
sruthi <sruthi.manian@gmail.com>
2016-11-07 15:57:06 +0530

    index.html

---
+++ app.js
@@ -0,0 +1,39 @@
+(function(){
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+LunchCheckController.$inject = ['$scope'];
+
+
+
+function LunchCheckController($scope)
+{
+		$scope.list="";
+	    $scope.message="";
+		$scope.check=function()
+		{
+			var count=0;
+		    var items=$scope.list.split(",");
+			for( var i=0; i<items.length;i++)
+			{
+				if(items[i].length!=0)
+				{
+					count=count+1;
+				}
+			}
+		
+		if(count==0)
+			$scope.message="Please enter data first";
+		else if(0<count<=3)
+			$scope.message= "Enjoy!!";
+		else
+			$scope.message= "Too much!";
+		
+		};
+
+}
+	
+})();
+
+
